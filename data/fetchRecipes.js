6713109c2aa5cbc2f55fa282c84c2e7cb63e5a30
Ho Yin Wong <hoyin.w.j@gmail.com>
2025-07-16 12:35:28 -0600

    added searching function

--- data/fetchRecipes.js
+++ data/fetchRecipes.js
@@ -1,4 +1,4 @@
-export async function fetchRechipes(query) {
+export async function fetchRecipes(query) {
     try {
         const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`)
         const data = await response.json()
--- src/main.js
+++ src/main.js
@@ -1,9 +1,26 @@
 import './style.css'
-import { fetchRechipes } from '../data/fetchRecipes.js'
+import { fetchRecipes } from '../data/fetchRecipes.js'
 import { renderRecipeCards } from '../data/renderCards.js'
 
+
+const searchInput = document.getElementById('search-input')
+const searchBtn = document.getElementById('search-btn')
+
+searchBtn.addEventListener('click', async () => {
+  const query = searchInput.value.trim()
+
+  let meals = ''
+
+  if (query) {
+    meals = await fetchRecipes(query)
+  }
+  if (meals) {
+    renderRecipeCards(meals)
+  }
+})
+
 async function init() {
-  const meals = await fetchRechipes('chicken')
+  const meals = await fetchRecipes('chicken')
   
   if (meals) {
     renderRecipeCards(meals)
